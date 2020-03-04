75c89b0375ad0e0dbea2b78632f553f03d3f6a8c
lechuzo32 <59780541+lechuzo32@users.noreply.github.com>
2020-03-04 11:47:15 -0300

    Cachematrix.R updated

--- cachematrix.R
+++ cachematrix.R
@@ -7,22 +7,22 @@
 
 makeCacheMatrix <- function(x = matrix()) {
     ## Initializing
-    a <- NULL
+    save_a <- NULL
     ## Defining a function to save values on Cache, special operator "<<-"
     set <- function(y) {
         ## Saving on Cache
-        x <<- y
+        save_x <<- y
         ## Initializing
-        a <<- NULL
+        save_a <<- NULL
     }
     
     # Defining "get" function
-    get <- function() x
+    get <- function() save_x
     # Defining "setinverse" function
     # Solve is an existing function on R
     setinverse <- function(solve) a <<- solve ## Remembre, the special operator "<<-"
     # Defining "getinverse" function
-    getinverse <- function() a
+    getinverse <- function() save_a
 
     # Menu list, this four operations are used to work with the matrix x
     list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
@@ -40,24 +40,24 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Returns the inverse matrix of x, usaing one of the operatios which are defined
     ## in the Menu list, on makeCacheMatrix
-    a <- x$getinverse()
+    save_a <- x$getinverse()
 
     ## Checks if somthing has been changed
-    if(!is.null(a)) {
+    if(!is.null(save_a)) {
             message("getting cached data")
-            return(a)
+            return(save_a)
     }
 
     # Defining "get" function
-    b <- x$get()
+    save_b <- x$get()
 
     # Defining "setinverse" function
     # Solve is an existing fucntion on R
-    a <- solve(b, ...)
+    save_a <- solve(save_b, ...)
 
     ## Stores on Cache the computed value of the inverse matrx
-    x$setinverse(a)
+    x$setinverse(save_a)
 
     # Returns inverse matrix
-    a
+    save_a
 }
